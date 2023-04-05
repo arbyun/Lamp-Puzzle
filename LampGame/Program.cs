628273f69326daeba3f449878c8f78852065f704
Daniela Castro <someonealiveh@gmail.com>
2023-04-05 17:47:56 +0100

    Update Main program to include lamps

--- LampGame/Program.cs
+++ LampGame/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using LampGame.scripts;
 
 namespace LampGame
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private Lamp _lamp01;
+        private Lamp _lamp02;
+        private Lamp _lamp03;
+        private int _numberOfTries;
+        private string[] _buttonList;
+        
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Lamp Puzzle.");
+        }
+
+        /// <summary>
+        /// Method to get the input from the user
+        /// </summary>
+        /// <param name="button">Theoretical button pressed</param>
+        private void PressButton(string button)
+        {
+            
         }
     }
 }
