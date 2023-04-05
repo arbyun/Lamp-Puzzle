1a3d85f58931ba50b9218638f2696dca060a318c
Daniela Castro <someonealiveh@gmail.com>
2023-04-05 17:46:51 +0100

    Create class for the lamps

---
+++ LampGame/scripts/Lamp.cs
@@ -0,0 +1,22 @@
+﻿namespace LampGame.scripts;
+
+public class Lamp
+{
+    public States State = States.Off;
+
+    /// <summary>
+    /// Method to switch the lamp's state
+    /// </summary>
+    private void SwitchState()
+    {
+        
+    }
+
+    /// <summary>
+    /// A visual indicator to show the Lamp's state to the user
+    /// </summary>
+    internal void DrawSelf()
+    {
+        
+    }
+}
